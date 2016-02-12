109d8b8e5d67471a865774f617eddac11309ff4b
xukai82@gmail.com <xukai82@gmail.com>
2016-02-12 16:00:52 -0500

    minor refactor

--- app/controllers/nupack_controller.rb
+++ app/controllers/nupack_controller.rb
@@ -6,23 +6,27 @@ class NupackController < ApplicationController
 		numOfPeople = params['numOfPeople'].to_i
 		material = params['material']
 
-		# Here I try to simplify the case a bit.
-		# I would implement a helper function to get the type for the material in real world case. 
-		puts case material
-		when 'food'
-			materialMarkup = 0.13
-		when 'drugs'
-		  	materialMarkup = 0.075
-		when 'electronics'
-		  	materialMarkup = 0.02 
-		else
-			materialMarkup = 0
-		end		
+		materialMarkup = get_material_markup(material)
 
 		totalCost = basePrice * 1.05 * (1 + 0.012*numOfPeople + materialMarkup)
 		totalCost = totalCost.round(2)
 
   		render :json => {:totalCost => totalCost}
+	end
+
+	def get_material_markup(material = '')
+		# Here I try to simplify the case a bit.
+		# I would implement a helper function to get the type for the material in real world case. 
+		case material
+		when 'food'
+			 0.13
+		when 'drugs'
+		  	0.075
+		when 'electronics'
+		  	0.02 
+		else
+			0
+		end		
 	end
 
 end
